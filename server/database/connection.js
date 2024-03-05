b49dba102e72d34f0bb5cab3db55422d24d246d8
Samra Harbi <samra.harbi78@gmail.com>
2024-03-05 08:38:58 +0100

    connecting database-frontend create

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,13 +1,22 @@
 const mongoose = require('mongoose')
 const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+  'mongodb://localhost/argentBankDB'
+
+// module.exports = async () => {
+//   try {
+//     await mongoose.connect(databaseUrl, {})
+//     console.log('Database successfully connected')
+//   } catch (error) {
+//     console.error(`Database Connectivity Error: ${error}`)
+//     throw new Error(error)
+//   }
+// }
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
-    console.log('Database successfully connected')
+      await mongoose.connect(process.env.DATABASE_URL, {});
+      console.log("CONNECTED TO DATABASE SUCCESSFULLY");
   } catch (error) {
-    console.error(`Database Connectivity Error: ${error}`)
-    throw new Error(error)
+      console.error('COULD NOT CONNECT TO DATABASE:', error.message);
   }
-}
+};
